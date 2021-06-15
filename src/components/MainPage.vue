0000f2368d3568bedb6838e7de33e7758d7e7922
Rhegolie <rhegolie@gmail.com>
2021-06-15 16:33:19 +0200

    store

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>Start</div>
+    <div>{{test}}</div>
 
 </template>
 
 <script>
+    import {mapState, mapActions} from 'vuex'
+
     export default {
         name: 'MainPage',
+        data(){
+            return{}
+        },
+        computed: {
+            ...mapState({
+                test: state => state.test.test
+            })
+        },
+        methods: {
+            ...mapActions('test', ['getTest'])
+        },
+        created(){
+            this.getTest()
+        }
     }
 </script>
 
