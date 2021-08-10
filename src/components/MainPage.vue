d05e2cde4cb8c141d49106da78ba1d2a638bb7b4
Rhegolie <rhegolie@gmail.com>
2021-08-10 21:47:25 +0200

    empty pages

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -96,8 +96,37 @@
                     <b-table striped :items='countryTableData' :fields='countryTable'></b-table>
                 </div>
             </b-tab>
-            <b-tab title='Correlation' :title-link-class='linkClass(2)'>{{linkClass(2)}}</b-tab>
-            <b-tab title='Raport' title-link-class='text-info'>{{linkClass(3)}}</b-tab>
+            <b-tab title='Correlation' :title-link-class='linkClass(2)'>
+                <br>
+                <h1 style='width:1100px; margin:auto;text-align: justify;'>Strona w trakcie rozbudowy: zakładka pozwalajaca na wybranie czynnika (PKB, gini, populacja, gęstość zaludnienia, etc.) 
+                    i generująca wykresy porównawcze (przykładowo punktowe); możliwość generacji linii trendu oraz wyliczenie jej wzoru; 
+                    obliczenie współczynnika korelacji oraz r kwadrat; inne statystyki; tabela z dostepnymi danymi do pobrania.
+                </h1>
+            </b-tab>
+            <b-tab title='Raport' title-link-class='text-info'><br>
+                <b-container>
+                    <b-row>
+                        <b-col>
+                            <b-list-group style='width: 400px; text-align: left; margin-left:200px;'>
+                                <h3> Elements of the report</h3>
+                                <b-list-group-item v-for='(element, i) in raportFile' :key='i'>
+                                    {{element}}
+                                    <b-badge variant='danger' pill style='float: right;'>delete</b-badge>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </b-col>
+                        <b-col><br>
+                            <h4>Download report</h4>
+                            <b-button-group>
+                                <b-button variant='info'>pdf</b-button>
+                                <b-button variant='info'>doc</b-button>
+                                <b-button variant='info'>ppt</b-button>
+                            </b-button-group>
+                        </b-col>
+                    </b-row>
+                </b-container>
+                
+            </b-tab>
             <template #tabs-end>
                 <li role='presentation' class='nav-item align-self-center h3'>COVID-19 Analytics</li>
             </template>
@@ -135,7 +164,8 @@
                     },
                     responsive: true,
                     maintainAspectRatio: false
-                }
+                },
+                raportFile:['Total cases world map', 'Spain - description card', 'Spain - last 30 days chart', 'Total tests world map']
             }
         },
         computed: {
